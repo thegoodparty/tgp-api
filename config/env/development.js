4b7882bfcc8f7330ed4f6e8950c83893330aeace
Tomer Almog <tomeralmog@gmail.com>
2019-07-23 00:20:33 -0700

    fix: change config to custom

--- config/env/development.js
+++ config/env/development.js
@@ -332,6 +332,7 @@ module.exports = {
   custom: {
     baseUrl: 'https://example.com',
     internalEmailAddress: 'support@example.com',
+    adminEmails: [],
 
     // mailgunDomain: 'mg.example.com',
     // mailgunSecret: 'key-prod_fake_bd32301385130a0bafe030c',
