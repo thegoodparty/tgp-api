ddd475f1c3d9ca3fcc1c0676d686d599293c03ca
Tomer Almog <tomeralmog@gmail.com>
2019-11-29 18:09:15 -0800

    feat: changer email recipient to farhad

--- api/helpers/mailgun-sender.js
+++ api/helpers/mailgun-sender.js
@@ -38,8 +38,8 @@ module.exports = {
 
       mg.messages
         .create('sandboxbcb5d5c9a3034d638e5854b64c476b8b.mailgun.org', {
-          from: 'TGP APP - AMA <ask@thegoodparty.org>',
-          to: ['ask@thegoodparty.org'],
+          from: 'TGP APP - AMA <farhad@thegoodparty.org>',
+          to: ['farhad@thegoodparty.org'],
           subject: 'AMA form submitted on TGP App',
           text: inputs.message,
           html: html(inputs.message),
