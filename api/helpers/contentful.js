6dc138184861a05aab30abb0b8cee906615e2164
Tomer Almog <tomeralmog@gmail.com>
2019-12-02 22:32:45 -0800

    feat: events

--- api/helpers/contentful.js
+++ api/helpers/contentful.js
@@ -36,10 +36,11 @@ module.exports = {
 
 const mapResponse = items => {
   const mappedResponse = {};
-  console.log(JSON.stringify(items));
-  items.map((item, index) => {
+  // console.log(JSON.stringify(items));
+  items.map(item => {
     if (item && item.sys && item.sys.contentType && item.sys.contentType.sys) {
       const itemId = item.sys.contentType.sys.id;
+      const elementId = item.sys.id;
       if (itemId === 'appPartyScreen') {
         mappedResponse.appPartyScreen = mapAppPartyScreen(item.fields);
       } else if (itemId === 'candidate') {
@@ -57,11 +58,18 @@ const mapResponse = items => {
           mappedResponse.faqArticles = [];
         }
         // front end needs an id.
-        mappedResponse.faqArticles.push({ ...item.fields, id: index + '' });
+        mappedResponse.faqArticles.push({ ...item.fields, id: elementId });
+      } else if (itemId === 'event') {
+        if (!mappedResponse.events) {
+          mappedResponse.events = [];
+        }
+        mappedResponse.events.push(mapEvent(item.fields, elementId));
       }
     }
   });
-
+  // need to order the event chronologically and separate the past events.
+  mappedResponse.events.sort(compareEvents);
+  splitPastEvents(mappedResponse);
   return mappedResponse;
 };
 
@@ -87,4 +95,84 @@ const mapCandidate = fields => {
   }
 
   return flatResponse;
+};
+
+const mapEvent = (fields, id) => {
+  const flatResponse = {};
+  const {
+    title,
+    dateAndTime,
+    description,
+    timeZone,
+    eventDuration,
+    presenter,
+  } = fields;
+  if (presenter) {
+  }
+
+  flatResponse.id = id;
+  flatResponse.title = title;
+  flatResponse.dateAndTime = dateAndTime;
+  flatResponse.description = description;
+  flatResponse.timeZone = timeZone;
+  flatResponse.eventDuration = eventDuration;
+  if (presenter) {
+    const { name, title, avatarPhoto } = presenter.fields;
+    flatResponse.presenter = name;
+    flatResponse.presenterTitle = title;
+    if (avatarPhoto && avatarPhoto.fields && avatarPhoto.fields.file) {
+      flatResponse.avatarPhoto = avatarPhoto.fields.file.url;
+    }
+  }
+  return flatResponse;
+};
+
+const splitPastEvents = response => {
+  // at this point the events are already sorted from past to future.
+
+  const allEvents = response.events;
+  const events = [];
+  const pastEvents = [];
+
+  allEvents.map(event => {
+    const today = new Date();
+    const serverHoursOffset = today.getTimezoneOffset() / 60;
+
+    const timeZoneHours = timeZoneToHours(event.timeZone);
+    today.setHours(today.getHours() + timeZoneHours + serverHoursOffset);
+    const eventDate = new Date(event.dateAndTime);
+    if (eventDate < today) {
+      pastEvents.push(event);
+    } else {
+      events.push(event);
+    }
+  });
+  response.events = events;
+  response.pastEvents = pastEvents;
+};
+
+const compareEvents = (a, b) => {
+  const dateA = new Date(a.dateAndTime);
+  const dateB = new Date(b.dateAndTime);
+  if (dateA > dateB) {
+    return 1;
+  }
+  if (dateA < dateB) {
+    return -1;
+  }
+  return 0;
+};
+
+const timeZoneToHours = timezone => {
+  if (!timezone) {
+    return 0;
+  }
+  if (timezone === 'PST') {
+    return -8;
+  } else if (timezone === 'EST') {
+    return -5;
+  } else if (timezone == 'CST') {
+    return -6;
+  }
+  return 0;
 };
