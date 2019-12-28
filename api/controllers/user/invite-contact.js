1426a51952344a022369de8013c74a3f43702cef
Tomer Almog <tomeralmog@gmail.com>
2019-12-28 10:58:25 -0800

    feat: V0.1.81 first and last name for invite

--- api/controllers/user/invite-contact.js
+++ api/controllers/user/invite-contact.js
@@ -13,9 +13,16 @@ module.exports = {
       minLength: 10,
     },
 
-    name: {
-      description: "Contact's Name",
-      example: 'John Smith',
+    firstName: {
+      description: "Contact's First Name",
+      example: 'John',
+      required: true,
+      type: 'string',
+    },
+
+    lastName: {
+      description: "Contact's Last Name",
+      example: 'Smith',
       required: true,
       type: 'string',
     },
@@ -38,7 +45,7 @@ module.exports = {
       console.log('invite-contact1');
       const reqUser = this.req.user;
       console.log('invite-contact2');
-      const { phone, name } = inputs;
+      const { phone, firstName, lastName } = inputs;
       console.log('invite-contact3', phone, name);
 
       // await sails.helpers.sendSms(
@@ -48,7 +55,7 @@ module.exports = {
       console.log('invite-contact4');
       await sails.helpers.sendSms(
         `+1${reqUser.phone}`,
-        `${reqUser.name}: Hey ${name}, check out The Good Party! https://exp.host/@tgp-expo/tgp-native-apps`,
+        `${reqUser.name}: Hey ${firstName}, check out The Good Party! https://exp.host/@tgp-expo/tgp-native-apps`,
       );
       console.log('invite-contact5');
 
@@ -75,13 +82,15 @@ module.exports = {
       if (invitedPhone) {
         const invitedBy = JSON.parse(invitedPhone.invitedBy);
         if (!invitedBy.includes(reqUser.id)) {
-          invitedBy.push({ id: reqUser.id, name });
+          invitedBy.push({ id: reqUser.id, name: `${firstName} ${lastName}` });
         }
         await Invited.updateOne({ id: invitedPhone.id }).set({
           invitedBy: JSON.stringify(invitedBy),
         });
       } else {
-        const invitedBy = JSON.stringify([{ id: reqUser.id, name }]);
+        const invitedBy = JSON.stringify([
+          { id: reqUser.id, name: `${firstName} ${lastName}` },
+        ]);
         await Invited.create({ phone, invitedBy });
       }
       console.log('invite-contact8');
