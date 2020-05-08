61878c7edaec4920ca5429c51b1d319ed7c7ba7b
Tomer Almog <tomeralmog@gmail.com>
2020-05-07 17:43:58 -0700

    fix: pres. images

--- api/controllers/seed/seed-presidential-source.js
+++ api/controllers/seed/seed-presidential-source.js
@@ -23,42 +23,50 @@ module.exports = {
           name: 'Donald Trump',
           source:
             'https://ballotpedia.org/Donald_Trump_presidential_campaign,_2020',
+          image: 'http://assets.thegoodparty.org/candidates/trump.jpg',
         },
         {
           name: 'Joe Biden',
           source:
             'https://ballotpedia.org/Joe_Biden_presidential_campaign,_2020',
+          image: 'http://assets.thegoodparty.org/candidates/biden.jpg',
         },
         {
           name: 'Bernie Sanders',
           source:
             'https://ballotpedia.org/Bernie_Sanders_presidential_campaign,_2020',
+          image: 'http://assets.thegoodparty.org/candidates/sanders.jpg',
         },
         {
           name: 'Elizabeth Warren',
           source:
             'https://ballotpedia.org/Elizabeth_Warren_presidential_campaign,_2020',
+          image: 'http://assets.thegoodparty.org/candidates/warren.jpg',
         },
         {
           name: 'Andrew Yang',
           source:
             'https://ballotpedia.org/Andrew_Yang_presidential_campaign,_2020',
+          image: 'http://assets.thegoodparty.org/candidates/yang.jpg',
         },
         {
           name: 'Howie Hawkins',
           source: 'https://ballotpedia.org/Howie_Hawkins',
+          image: 'http://assets.thegoodparty.org/candidates/hawkins.jpg',
         },
         {
           name: 'Jacob Hornberger',
           source: 'https://ballotpedia.org/Jacob_Hornberger',
+          image: 'http://assets.thegoodparty.org/candidates/hornberger.jpg',
         },
       ];
       for (let i = 0; i < sources.length; i++) {
-        const cand = sources[i];
+        const { name, source, image } = sources[i];
         await PresidentialCandidate.updateOne({
-          name: cand.name,
+          name,
         }).set({
-          source: cand.source,
+          source,
+          image,
         });
       }
       return exits.success({
