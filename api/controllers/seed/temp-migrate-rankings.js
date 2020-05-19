75dda0bddd83f4c2f9355dc522fb811fe9d80916
Tomer Almog <tomeralmog@gmail.com>
2020-05-19 02:29:28 -0700

    fix: migrate script

--- api/controllers/seed/temp-migrate-rankings.js
+++ api/controllers/seed/temp-migrate-rankings.js
@@ -30,10 +30,6 @@ module.exports = {
           senateRank && senateRank !== '' ? JSON.parse(senateRank) : [];
         houseRank = houseRank && houseRank !== '' ? JSON.parse(houseRank) : [];
 
-        console.log('p', typeof presidentialRank);
-        console.log('s', typeof Object.values(senateRank));
-        console.log('h', Object.values(houseRank));
-
         const senateValues = Object.values(senateRank)[0] || [];
         const houseValues = Object.values(houseRank)[0] || [];
         await migrateChamber(presidentialRank, 'presidential', user);
@@ -55,50 +51,64 @@ module.exports = {
 
 const migrateChamber = async (oldRanking, chamber, user) => {
   for (let i = 0; i < oldRanking.length; i++) {
+    console.log(
+      `migrating chamber ${chamber}, oldRanking: ${oldRanking}, for user: ${user.name}`,
+    );
+    let candidate;
     if (chamber !== 'presidential') {
       // verify the candidate matches the user district
-      const candidate = await RaceCandidate.findOne({ id: oldRanking[i] });
+      candidate = await RaceCandidate.findOne({ id: oldRanking[i] });
       if (
         !candidate ||
         candidate.district !== user.districtNumber ||
         candidate.state !== user.shortState
       ) {
+        console.log(
+          `continue1: ${
+            candidate ? candidate.name : 'no cand'
+          }, user district: ${user.districtNumber}`,
+        );
         continue;
       }
+    } else {
+      candidate = await PresidentialCandidate.findOne({ id: oldRanking[i] });
+    }
 
-      // don't migrate non good candidates
-      let incumbentRaised = 50000000;
-      let incumbent;
-      if (chamber !== 'presidential') {
-        if (candidate.isIncumbent) {
-          incumbentRaised = candidate.raised;
-          ({ incumbent } = await sails.helpers.incumbentByDistrictHelper());
+    // don't migrate non good candidates
+    let incumbentRaised = 50000000;
+    let incumbent;
+    if (chamber !== 'presidential') {
+      if (candidate.isIncumbent) {
+        incumbentRaised = candidate.raised;
+        ({ incumbent } = await sails.helpers.incumbentByDistrictHelper());
+      } else {
+        if (chamber === 'senate') {
+          ({ incumbent } = await sails.helpers.incumbentByDistrictHelper(
+            candidate.state,
+          ));
         } else {
-          if (chamber === 'senate') {
-            ({ incumbent } = await sails.helpers.incumbentByDistrictHelper(
-              candidate.state,
-            ));
-          } else {
-            ({ incumbent } = await sails.helpers.incumbentByDistrictHelper(
-              candidate.state,
-              candidate.district,
-            ));
-          }
-          incumbentRaised = incumbent
-            ? incumbent.raised || incumbent.combinedRaised
-            : false;
-          incumbentRaised = incumbentRaised ? incumbentRaised / 2 : false;
+          ({ incumbent } = await sails.helpers.incumbentByDistrictHelper(
+            candidate.state,
+            candidate.district,
+          ));
         }
+        incumbentRaised = incumbent
+          ? incumbent.raised || incumbent.combinedRaised
+          : false;
+        incumbentRaised = incumbentRaised ? incumbentRaised / 2 : false;
       }
-      const { isGood } = await sails.helpers.goodnessHelper(
-        candidate,
-        chamber,
-        incumbentRaised,
-      );
+    }
+    const { isGood } = await sails.helpers.goodnessHelper(
+      candidate,
+      chamber,
+      incumbentRaised,
+    );
 
-      if (isGood === false) {
-        continue;
-      }
+    if (isGood === false) {
+      console.log(
+        `not migrating non good candidates ranking. candidate: ${candidate.name}`,
+      );
+      continue;
     }
 
     await Ranking.create({
