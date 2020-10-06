a480bb10873d3c0de799e01fc68e27e0e1e2d904
tomer-tgp <tomer@thegoodparty.org>
2020-10-06 00:03:41 -0700

    chore: seed full

--- config/env/development.js
+++ config/env/development.js
@@ -48,7 +48,7 @@ module.exports = {
       adapter: require('sails-postgresql'),
       url: 'postgresql://postgres:postgres@localhost:5432/tgp-local',
       // url: 'postgresql://local:test123@localhost:5432/tgp-local',
-      
+
       // adapter: 'sails-mysql',
       // url: 'mysql://user:password@host:port/database',
       //--------------------------------------------------------------------------
