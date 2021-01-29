e578f42056b0b85d93a166cfc8fdf1f1acedf153
tomer-tgp <tomer@thegoodparty.org>
2021-01-28 23:15:47 -0800

    feat: upload compared candidates images

--- api/controllers/newCandidates/update.js
+++ api/controllers/newCandidates/update.js
@@ -5,6 +5,8 @@
  * @help        :: See https://sailsjs.com/documentation/concepts/actions-and-controllers
  */
 
+const fileExt = 'jpeg';
+
 module.exports = {
   friendlyName: 'Update Candidate',
 
@@ -29,7 +31,6 @@ module.exports = {
   },
   fn: async function(inputs, exits) {
     try {
-      const fileExt = 'jpeg';
       const { candidate } = inputs;
       const { imageBase64, id } = candidate;
 
@@ -40,13 +41,13 @@ module.exports = {
         .replace(/ /g, '-')}`;
       // upload the image
       let image;
+      const assetsBase =
+        sails.config.custom.assetsBase || sails.config.assetsBase;
+      const uuid = Math.random()
+        .toString(36)
+        .substring(2, 8);
       if (imageBase64) {
-        const assetsBase =
-          sails.config.custom.assetsBase || sails.config.assetsBase;
         const cleanBase64 = imageBase64.replace(/^data:image\/.*;base64,/, '');
-        const uuid = Math.random()
-          .toString(36)
-          .substring(2, 8);
 
         const fileName = `${name}-${uuid}.${fileExt}`;
 
@@ -62,6 +63,7 @@ module.exports = {
         );
         image = `https://${assetsBase}/candidates/${fileName}`;
       }
+      await uploadComparedImage(candidate);
 
       const cleanCandidate = {
         ...candidate,
@@ -86,4 +88,42 @@ module.exports = {
       return exits.badRequest({ message: 'Error registering candidate.' });
     }
   },
+};
+
+const uploadComparedImage = async candidate => {
+  const { comparedCandidates } = candidate;
+  if (!comparedCandidates) {
+    return;
+  }
+  const { uploadedImages, candidates } = comparedCandidates;
+  if (!uploadedImages) {
+    return;
+  }
+  const assetsBase = sails.config.custom.assetsBase || sails.config.assetsBase;
+
+  for (let i = 0; i < candidates.length; i++) {
+    if (uploadedImages[i]) {
+      const { base64 } = uploadedImages[i];
+      if (base64) {
+        const uuid = Math.random()
+          .toString(36)
+          .substring(2, 8);
+        const cleanBase64 = base64.replace(/^data:image\/.*;base64,/, '');
+
+        const fileName = `${candidates[i].name}-${uuid}.${fileExt}`;
+        const data = {
+          Key: fileName,
+          ContentEncoding: 'base64',
+          ContentType: `image/${fileExt}`,
+        };
+        await sails.helpers.s3Uploader(
+          data,
+          `${assetsBase}/candidates`,
+          cleanBase64,
+        );
+        candidates[i].image = `https://${assetsBase}/candidates/${fileName}`;
+      }
+    }
+  }
+  delete comparedCandidates.uploadedImages;
 };
