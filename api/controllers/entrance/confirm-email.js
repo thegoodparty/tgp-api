1a3578e0c14dad6c9077b15b99a3dfdaf39bb54e
KING DEV <kd19850511@gmail.com>
2021-02-25 15:23:25 -0500

    feat: add post-verification email (#31)

--- api/controllers/entrance/confirm-email.js
+++ api/controllers/entrance/confirm-email.js
@@ -27,7 +27,7 @@ module.exports = {
     },
   },
 
-  fn: async function(inputs, exits) {
+  async fn(inputs, exits) {
     const { email, token } = inputs;
     const lowerCaseEmail = email.toLowerCase();
 
@@ -74,6 +74,70 @@ module.exports = {
       }).populate('cds');
       userWithZip.zipCode = zipCode;
 
+      const subject = `You're verified — here's what else you can do to help`;
+      const message = `<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+        <tbody>
+          <tr>
+            <td>
+              <p
+                style="
+                  font-family: Arial, sans-serif;
+                  font-size: 18px;
+                  line-height: 26px;
+                  color: ##555555;
+                  margin: 0;
+                  text-align: left;
+                "
+              >
+                Hi ${user.firstName || user.name}!<br /><br />
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p
+                style="
+                  font-family: Arial, sans-serif;
+                  font-size: 18px;
+                  line-height: 26px;
+                  color: ##555555;
+                  margin: 0;
+                  text-align: left;
+                "
+              >
+              Your email has been verified and we will update you with campaign updates.
+              If you haven’t done so, you should also sign up on the candidates site and follow them on socials to get their direct updates too:
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p
+                style="
+                  font-family: Arial, sans-serif;
+                  font-size: 18px;
+                  line-height: 26px;
+                  color: ##555555;
+                  margin: 0;
+                  text-align: left;
+                "
+              >
+              
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      `;
+
+      const messageHeader = '';
+      await sails.helpers.mailgunSender(
+        lowerCaseEmail,
+        user.name,
+        subject,
+        messageHeader,
+        message,
+      );
       return exits.success({
         user: userWithZip,
         token,
