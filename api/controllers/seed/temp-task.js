c19c683d957fdb28b856e76d3a3c407b412c4eb6
talmog <talmog@spokeo.com>
2021-04-12 22:36:56 -0700

    chore: logs for debug

--- api/controllers/seed/temp-task.js
+++ api/controllers/seed/temp-task.js
@@ -18,13 +18,20 @@ module.exports = {
 
   fn: async function(inputs, exits) {
     try {
+      console.log('temp-task1');
       const candidates = await Candidate.find();
+      console.log('temp-task2');
       for (let i = 0; i < candidates.length; i++) {
+        console.log('temp-task3 lpop candidates[i]', candidates[i]);
         const { data } = candidates[i];
+        console.log('temp-task4', data);
         if (data) {
           let parsed = JSON.parse(data);
+          console.log('temp-task5');
           const { image } = parsed;
+          console.log('temp-task6', image);
           if (image) {
+            console.log('temp-task7');
             const newImage = image.replace(
               'https://assets.thegoodparty.org',
               'https://assets.goodparty.org',
@@ -33,14 +40,17 @@ module.exports = {
             console.log('newImage', newImage);
 
             parsed.image = newImage;
+            console.log('temp-task8');
             const newData = JSON.stringify(parsed);
+            console.log('temp-task9');
             await Candidate.updateOne({ id: candidates[i].id }).set({
               data: newData,
             });
           }
         }
       }
-      return exits.success({});
+      console.log('temp-task10');
+      return exits.success({ message: 'seeded' });
     } catch (e) {
       console.log(e);
       return exits.badRequest({
