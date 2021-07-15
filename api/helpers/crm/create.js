0f7427714f1d4019a070526f64dc84fbc28b44bc
talmog <talmog@spokeo.com>
2021-07-14 21:54:00 -0700

    chore: add user to nation builder CRM

--- api/helpers/crm/create.js
+++ api/helpers/crm/create.js
@@ -21,57 +21,52 @@ module.exports = {
     },
   },
   fn: async function(inputs, exits) {
-    try {
-      const { user } = inputs;
+    const { user } = inputs;
 
-      const url = `https://goodparty.nationbuilder.com/api/v1/people?access_token=${nationBuilderAccessToken}`;
-      const body = {
-        person: {
-          first_name: user.name
-            .split(' ')
-            .slice(0, -1)
-            .join(' '),
-          last_name: user.name
-            .split(' ')
-            .slice(-1)
-            .join(' '),
-          email: user.email,
-          phone: user.phone,
-          profile_image_url_ssl: user.avatar,
-        },
+    const url = `https://goodparty.nationbuilder.com/api/v1/people?access_token=${nationBuilderAccessToken}`;
+    const body = {
+      person: {
+        first_name: user.name
+          .split(' ')
+          .slice(0, -1)
+          .join(' '),
+        last_name: user.name
+          .split(' ')
+          .slice(-1)
+          .join(' '),
+        email: user.email,
+        phone: user.phone,
+        profile_image_url_ssl: user.avatar,
+      },
+    };
+
+    const res = await axios({
+      url,
+      method: 'POST',
+      data: body,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    // update user record with the id from the crm
+
+    let metaData;
+    const nationBuilderId = res.data.person.id;
+    if (user.metaData && user.metaData !== '') {
+      const parsedMeta = JSON.parse(user.metaData);
+      metaData = {
+        ...parsedMeta,
+        nationBuilderId,
       };
-
-      const res = await axios({
-        url,
-        method: 'POST',
-        data: body,
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      });
-
-      // update user record with the id from the crm
-
-      let metaData;
-      const nationBuilderId = res.data.person.id;
-      if (user.metaData && user.metaData !== '') {
-        const parsedMeta = JSON.parse(user.metaData);
-        metaData = {
-          ...parsedMeta,
-          nationBuilderId,
-        };
-      } else {
-        metaData = { nationBuilderId };
-      }
-
-      await User.updateOne({ id: user.id }).set({
-        metaData: JSON.stringify(metaData),
-      });
-      return exits.success('ok');
-    } catch (err) {
-      console.log('error at helpers/crm/create', err);
-      return exits.badRequest({ message: 'Error', error: err });
+    } else {
+      metaData = { nationBuilderId };
     }
+
+    await User.updateOne({ id: user.id }).set({
+      metaData: JSON.stringify(metaData),
+    });
+    return exits.success('ok');
   },
 };
