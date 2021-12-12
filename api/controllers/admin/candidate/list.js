8e5f59fd51ded62b426df8a4000232bf09ea71be
tomer-tgp <tomer@thegoodparty.org>
2021-12-12 11:50:20 -0800

    feat: add user to admin candidate list

--- api/controllers/admin/candidate/list.js
+++ api/controllers/admin/candidate/list.js
@@ -3,11 +3,7 @@ module.exports = {
 
   description: 'admin call for getting all candidates',
 
-  inputs: {
-    chamber: {
-      type: 'string',
-    },
-  },
+  inputs: {},
 
   exits: {
     success: {
@@ -22,77 +18,15 @@ module.exports = {
 
   fn: async function(inputs, exits) {
     try {
-      const { chamber } = inputs;
       let candidates;
-      if (chamber === 'presidential') {
-        candidates = await PresidentialCandidate.find({
-          isActive: true,
-        }).sort([{ isIncumbent: 'DESC' }, { combinedRaised: 'DESC' }]);
-      } else if (chamber === 'local') {
-        candidates = await Candidate.find().sort([{ updatedAt: 'DESC' }]);
-        candidates = candidates.map(candidate => JSON.parse(candidate.data));
-        return exits.success({
-          candidates,
-        });
-      } else {
-        let upperChamber = 'House';
-        if (chamber === 'senate') {
-          upperChamber = 'Senate';
-        }
-        const incumbents = await Incumbent.find({
-          isActive: true,
-          chamber: upperChamber,
-        }).sort([{ raised: 'DESC' }]);
-        const raceCand = await RaceCandidate.find({
-          isActive: true,
-          chamber: upperChamber,
-        }).sort([{ raised: 'DESC' }]);
-        incumbents.forEach(incumbent => (incumbent.isIncumbent = true));
-        candidates = [...incumbents, ...raceCand];
-      }
 
-      for (let i = 0; i < candidates.length; i++) {
-        const candidate = candidates[i];
-        const { state, district } = candidate || {};
-        let incumbent;
+      candidates = await Candidate.find()
+        .populate('user')
+        .sort([{ updatedAt: 'DESC' }]);
 
-        let incumbentRaised = 50000000;
-        if (chamber !== 'presidential') {
-          if (candidate.isIncumbent) {
-            incumbentRaised = candidate.raised;
-            ({ incumbent } = await sails.helpers.incumbentByDistrictHelper());
-          } else {
-            if (chamber === 'senate') {
-              ({ incumbent } = await sails.helpers.incumbentByDistrictHelper(
-                state,
-              ));
-            } else {
-              ({ incumbent } = await sails.helpers.incumbentByDistrictHelper(
-                state,
-                district ? parseInt(district, 10) : district,
-              ));
-            }
-            incumbentRaised = incumbent
-              ? incumbent.raised || incumbent.combinedRaised
-              : false;
-            incumbentRaised = incumbentRaised ? incumbentRaised / 2 : false;
-          }
-        }
-
-        const {
-          isGood,
-          isBigMoney,
-          isMajor,
-        } = await sails.helpers.goodnessHelper(
-          candidate,
-          chamber,
-          incumbentRaised,
-        );
-        candidate.isGood = isGood;
-        candidate.isBigMoney = isBigMoney;
-        candidate.isMajor = isMajor;
-      }
-
+      candidates = candidates.map(candidate => {
+        return { ...JSON.parse(candidate.data), user: candidate.user };
+      });
       return exits.success({
         candidates,
       });
