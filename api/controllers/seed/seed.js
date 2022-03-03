bcda895a8e5733de6aa092a9f0e79ba24141ba56
webdeveloper001 <kd19850511@gmail.com>
2022-03-03 17:06:34 -0500

    update seed

--- api/controllers/seed/seed.js
+++ api/controllers/seed/seed.js
@@ -21,130 +21,47 @@ module.exports = {
 
   async fn(inputs, exits) {
     try {
-      const appBase = sails.config.custom.appBase || sails.config.appBase;
-      const listName =
-        appBase === 'https://goodparty.org'
-          ? 'Good Party'
-          : 'Good Party Dev';
-      const { lists } = await mailchimp.lists.getAllLists();
-      console.log('lists:', lists);
-      const tgpList = lists.find(list => list.name === listName);
-
-      let users = await User.find();
-      let candidates = await Candidate.find();
-      let candidateTags = {};
-      candidates.forEach(candidate => {
-        const { race } = JSON.parse(candidate.data);
-        let { firstName, lastName, id } = candidate;
-        const tag = `${firstName} ${lastName} for ${race} ### ${id}`;
-        candidateTags[candidate.id] = tag;
-        console.log('TAG: ', tag);
+      // console.log(await CandidateIssue.find({}));
+      const oldCandidateIssueList = await CandidateIssue.find({});
+      const allIssueTopics = await IssueTopic.find({});
+      const issueTopicList = {};
+      allIssueTopics.forEach(issueTopic => {
+        issueTopicList[issueTopic.id] = issueTopic;
       });
-      let offset = 0;
-      let memberList = [];
-      while (true) {
-        console.log('while iteration', offset);
-        let { members } = await mailchimp.lists.getListMembersInfo(tgpList.id, {
-          count: 500,
-          offset,
-        });
-
-        memberList = [...memberList, ...members];
-        offset += 500;
-        if (members.length === 0) {
-          break;
-        }
-      }
-      console.log('memberList.length', memberList.length);
-      // const { members } = await mailchimp.lists.getListMembersInfo(tgpList.id, { count: 1000, offset: 0 });
-      // console.log(members.map(member => member.id));
-      // const response = await mailchimp.lists.addListMember(tgpList.id, {
-      //   email_address: users[0].email,
-      //   status: 'subscribed',
-      //   merge_fields: {
-      //     FNAME: users[0].name.split(' ')[0].trim(),
-      //     LNAME: users[0].name.replace(users[0].name.split(' ')[0], '').trim(),
-      //   },
-      // });
-      const candidateSupports = await Support.find();
-      for (let i = 0; i < users.length; i++) {
-        console.log(users[i].email, users[i].name);
-        // const candidateSupports = await Support.find({
-        //   candidate: users[i].id
-        // });
-        const supports = candidateSupports.filter(
-          support => support.user === users[i].id,
-        );
-        const tags = supports
-          .map(support => candidateTags[support.candidate])
-          .filter(item => !!item);
-        console.log('user tags', tags);
-        const member = memberList.find(
-          member => member.email_address === users[i].email,
-        );
-        if (member) {
-          console.log('Found', member);
-          // console.log(
-          //   memberList.find(member => member.email_address === users[i].email),
-          // );
-          const fname = users[i].name.split(' ')[0].trim(),
-            lname = users[i].name
-              .replace(users[i].name.split(' ')[0], '')
-              .trim();
-          if (
-            member.merge_fields.FNAME === fname &&
-            member.merge_fields.LNAME === lname
-          ) {
-            console.log('continue', fname, lname);
-            continue;
-          } else {
-            const subscriberHash = md5(users[i].email);
-            await mailchimp.lists.updateListMember(tgpList.id, subscriberHash, {
-              email_address: users[i].email,
-              status: 'subscribed',
-              merge_fields: {
-                FNAME: users[i].name.split(' ')[0].trim(),
-                LNAME: users[i].name
-                  .replace(users[i].name.split(' ')[0], '')
-                  .trim(),
-                ADDRESS: 'None',
-              },
-              tags,
-            });
-            console.log('added ', users[i].email);
-            continue;
-          }
-        }
-        // try {
-        //   const subscriberHash = md5(users[i].email);
-        //   await mailchimp.lists.deleteListMemberPermanent(
-        //     tgpList.id,
-        //     subscriberHash,
-        //   );
-        // } catch (e) {
-        //   console.log(e);
-        // }
-        try {
-          console.log('before addListMember', users[i].email);
-          await mailchimp.lists.addListMember(tgpList.id, {
-            email_address: users[i].email,
-            status: 'subscribed',
-            merge_fields: {
-              FNAME: users[i].name.split(' ')[0].trim(),
-              LNAME: users[i].name
-                .replace(users[i].name.split(' ')[0], '')
-                .trim(),
-            },
-            tags,
+      
+      for (let i = 0; i < oldCandidateIssueList.length; i++) {
+        const oldCandidateIssue = oldCandidateIssueList[i];
+        for (let j = 0; j < oldCandidateIssueList[i].data.length; j++) {
+          
+          const item = oldCandidateIssue.data[j];
+          const candidate = await Candidate.findOne({
+            id: oldCandidateIssue.candidate,
           });
-          console.log('after addListMember', users[i].email);
-        } catch (e) {
-          console.log('error addListMember', users[i].email);
-          console.log(e);
+          
+          const topic = issueTopicList[item.topicId];
+          const candidateIssueItem = await CandidateIssueItem.findOrCreate(
+            {
+              topic: topic.id,
+              candidate: candidate.id,
+            },
+            {
+              candidate: candidate.id,
+              topic: topic.id,
+            },
+          );
+          await CandidateIssueItem.updateOne({
+            id: candidateIssueItem.id,
+          }).set({
+            candidate: candidate.id,
+            topic: topic.id,
+            positionId: item.positionId,
+            description: item.description,
+            websiteUrl: item.websiteUrl,
+            status: oldCandidateIssue.status,
+          });
         }
       }
-      // console.log('Hello World')
-      return exits.success(candidateTags);
+      return exits.success(oldCandidateIssueList);
     } catch (e) {
       console.log('Error in mailchimp seed', e);
       return exits.success({
