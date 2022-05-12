2e6cf012687bdbc1d383761ce2e57112cbdb6837
tomer-tgp <tomer@thegoodparty.org>
2022-05-11 21:04:32 -0700

    fix: create candidate via admin

--- api/controllers/newCandidates/create.js
+++ api/controllers/newCandidates/create.js
@@ -69,6 +69,7 @@ module.exports = {
         firstName: candidate.firstName.trim(),
         lastName: candidate.lastName.trim(),
         chamber: 'local',
+        contact: {},
         image,
       };
 
