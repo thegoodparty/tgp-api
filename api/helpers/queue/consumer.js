05de72ae4820381332983f7d835869de9e9a4119
tomer-tgp <tomer@thegoodparty.org>
2023-04-27 20:22:34 -0700

    chore: debug 7

--- api/helpers/queue/consumer.js
+++ api/helpers/queue/consumer.js
@@ -145,12 +145,12 @@ async function handleGenerateCampaignPlan(message) {
       console.log('error', e.data.error);
     } else {
       await sails.helpers.errorLoggerHelper(
-        'error at AI queue consumer debug: ',
-        e,
-      );
-      await sails.helpers.errorLoggerHelper(
         'error at AI queue consumer. Queue Message: ',
         message,
+      );
+      await sails.helpers.errorLoggerHelper(
+        'error at AI queue consumer debug: ',
+        e,
       );
     }
   }
