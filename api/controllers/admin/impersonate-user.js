490cdcee68bad90ddee7fb98366101ba4cc848b3
Taylor Murray <taylor@goodparty.org>
2023-07-06 21:25:59 -0700

    fix: cleanup code.

--- api/controllers/admin/impersonate-user.js
+++ api/controllers/admin/impersonate-user.js
@@ -27,52 +27,12 @@ module.exports = {
   fn: async function (inputs, exits) {
     try {
       const { email } = inputs;
-      const authorizationHeader = this.req.headers.authorization;
-
-      let bearerToken;
-      // get the bearerToken from the request headers.
-      if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
-        bearerToken = authorizationHeader.substring('Bearer '.length);
-      } else {
-        return res.status(401).json({ error: 'Invalid authorization header' });
-      }
-
-      if (!email && !bearerToken) {
-        return exits.badRequest({
-          message: 'a valid session and email are required',
-        });
-      }
-
-      // decode the requesting users' token.
-      let userEmail;
-      const decodedToken = await jwt.decode(bearerToken);
-      if (
-        decodedToken &&
-        typeof decodedToken === 'object' &&
-        decodedToken.data.email !== undefined
-      ) {
-        userEmail = decodedToken.data.email;
-      }
-
-      if (!userEmail) {
-        return exits.badRequest({
-          message: 'invalid session',
-        });
-      }
-
-      // ensure the requesting user exists and has admin rights.
-      let user = await User.findOne({ email: userEmail, isAdmin: true });
-      if (!user) {
-        return exits.badRequest({
-          message: 'forbidden',
-        });
-      }
 
       // get the impersonateUser object by email.
       let impersonateUser = await User.findOne({ email });
-      if (!user) {
+      if (!impersonateUser) {
         return exits.badRequest({
-          message: 'forbidden',
+          message: 'invalid user',
         });
       }
 
