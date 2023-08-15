b405d0e27c053ea04872791c825c82e0e4f77809
Taylor Murray <taylor@goodparty.org>
2023-08-15 12:05:48 -0700

    fix: update legacy aiContent format on campaign.

--- api/controllers/campaign/onboarding/find-by-user.js
+++ api/controllers/campaign/onboarding/find-by-user.js
@@ -5,6 +5,11 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const camelToSentence = (text) => {
+  const result = text.replace(/([A-Z])/g, ' $1');
+  return result.charAt(0).toUpperCase() + result.slice(1);
+};
+
 module.exports = {
   friendlyName: 'Find Campaign associated with user',
 
@@ -29,8 +34,32 @@ module.exports = {
         user: user.id,
       });
       let campaign = false;
+      let campaignId = 0;
       if (campaigns && campaigns.length > 0) {
         campaign = campaigns[0].data;
+        campaignId = campaigns[0].id;
+      }
+
+      // fix any old style aiContent.
+      let updated = false;
+      if (campaign.aiContent) {
+        for (const key in campaign.aiContent) {
+          if (campaign.aiContent.hasOwnProperty(key)) {
+            if (typeof campaign.aiContent[key] === 'string') {
+              updated = true;
+              campaign.aiContent[key] = {
+                name: camelToSentence(key),
+                content: campaign.aiContent[key],
+                updatedAt: new Date().valueOf(),
+              };
+            }
+          }
+        }
+      }
+      if (updated === true) {
+        await Campaign.updateOne({
+          id: campaignId,
+        }).set({ data: campaign });
       }
 
       return exits.success({
