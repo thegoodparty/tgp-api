d9e6e2ac9284fe3b505fdad4e3b646600300dc11
tomer-tgp <tomer@thegoodparty.org>
2023-08-17 21:47:25 -0700

    feat: WEB-1086

---
+++ api/controllers/content/blog-articles-titles.js
@@ -0,0 +1,39 @@
+module.exports = {
+  inputs: {},
+
+  exits: {
+    success: {
+      description: 'ok',
+      responseType: 'ok',
+    },
+    badRequest: {
+      description: 'Error',
+      responseType: 'badRequest',
+    },
+  },
+
+  fn: async function (inputs, exits) {
+    try {
+      // const { key, subKey, subValue, limit, deleteKey } = inputs;
+      const contents = await CmsContent.find();
+      const content = JSON.parse(contents[1].content);
+
+      const keyContent = content.blogArticles;
+      const titles = keyContent.map((article) => {
+        return { title: article.title, slug: article.slug };
+      });
+      return exits.success({
+        titles,
+      });
+    } catch (err) {
+      console.log(err);
+      await sails.helpers.errorLoggerHelper(
+        'Error at content/landing-page-content',
+        err,
+      );
+      return exits.badRequest({
+        message: 'Content fetch failed.',
+      });
+    }
+  },
+};
