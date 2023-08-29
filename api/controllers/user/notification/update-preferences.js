ef9b5c8848fa47d95b73ec050a46d85bd81392d0
Taylor Murray <taylor@goodparty.org>
2023-08-29 12:53:18 -0700

    feat: unsubscribe from marketing emails.

--- api/controllers/user/notification/update-preferences.js
+++ api/controllers/user/notification/update-preferences.js
@@ -1,3 +1,7 @@
+const axios = require('axios');
+const hubSpotToken =
+  sails.config.custom.hubSpotToken || sails.config.hubSpotToken;
+
 module.exports = {
   inputs: {
     metaData: {
@@ -23,6 +27,23 @@ module.exports = {
       const { metaData } = inputs;
 
       const updated = await User.updateOne({ id: user.id }).set({ metaData });
+      const metaDataObj = JSON.parse(metaData);
+
+      if (
+        metaDataObj.hasOwnProperty('marketingEmails') &&
+        metaDataObj.marketingEmails === false
+      ) {
+        // https://legacydocs.hubspot.com/docs/methods/email/update_status
+        let email = user.email;
+        const url = `https://api.hubapi.com/email/public/v1/subscriptions/${email}`;
+        console.log('url', url);
+        const payload = { unsubscribeFromAll: true };
+        await axios.put(url, payload, {
+          headers: {
+            Authorization: `Bearer ${hubSpotToken}`,
+          },
+        });
+      }
 
       return exits.success({
         user: updated,
