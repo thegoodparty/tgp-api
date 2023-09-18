4b61cc8b1cdb443407e15f278208bd8d4b0ff0e0
tomer-tgp <tomer@thegoodparty.org>
2023-09-18 23:48:13 +0300

    chore: WEB-1197

--- api/controllers/user/notification/cron/update-tracker.js
+++ api/controllers/user/notification/cron/update-tracker.js
@@ -104,9 +104,9 @@ module.exports = {
           if (canText) {
             await sails.helpers.sms.sendSms(
               campaign.user.phone,
-              `Time to update your Campaign Tracker!
+              `Hi, this is the Good Party Team. It's time to update your Campaign Tracker!
 How many doors did you or your team knock on this week?
-
+              
 Text STOP to unsubscribe`,
             );
             // save status to user meta data
