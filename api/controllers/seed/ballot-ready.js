c3da55c96365790f54f4c748ee879f4b6e4ffc7a
tomer-tgp <tomer@thegoodparty.org>
2023-10-29 19:13:27 -0700

    chore: remove state query

--- api/controllers/seed/ballot-ready.js
+++ api/controllers/seed/ballot-ready.js
@@ -4,17 +4,13 @@ module.exports = {
       type: 'string',
       required: true,
     },
-    state: {
-      type: 'string',
-      required: true,
-    },
   },
 
   exits: {},
 
   async fn(inputs, exits) {
     try {
-      const { zip, state } = inputs;
+      const { zip } = inputs;
 
       const query = `
       query {
@@ -26,8 +22,7 @@ module.exports = {
             electionDay: {
               gt: "2023-10-29" 
               lt: "2025-01-01" 
-            }
-            state: "${state.toUpperCase()}"
+            }            
             mtfcc: "G4110"
             level: CITY
           }
