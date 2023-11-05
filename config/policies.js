85aa324f2d6b4e24b974f6930de4b4bac6ea93c4
tomer-tgp <tomer@thegoodparty.org>
2023-11-05 12:55:38 -0800

    feat: edit top issues from candidate page

--- config/policies.js
+++ config/policies.js
@@ -45,6 +45,7 @@ module.exports.policies = {
   'topIssues/topIssue/list': true,
   'topIssues/position/list': true,
   'topIssues/candidatePosition/find-by-candidate': true,
+  'topIssues/candidatePosition/find-by-campaign': true,
   'visit/*': true,
   'newCandidates/application/upload-image': true, // for guest applications
   'socialListening/*': true,
