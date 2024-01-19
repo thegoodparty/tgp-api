0ff75b93c389ebcf116f690e22c9abde6612e46e
Taylor Murray <taylor@goodparty.org>
2024-01-18 16:49:44 -0800

    fix: log helper.

--- api/helpers/log.js
+++ api/helpers/log.js
@@ -22,21 +22,21 @@ module.exports = {
     try {
       const { arg1, arg2, arg3 } = inputs;
       if (arg1 && arg2 && arg3) {
-        log([`[${inputs.slug}]`, arg1, arg2, arg3]);
+        console.log([`[${inputs.slug}]`, arg1, arg2, arg3]);
       } else if (arg1 && arg2) {
-        log([`[${inputs.slug}]`, arg1, arg2]);
+        console.log([`[${inputs.slug}]`, arg1, arg2]);
       } else if (arg1) {
-        log([`[${inputs.slug}]`, arg1]);
+        console.log([`[${inputs.slug}]`, arg1]);
       }
     } catch (e) {}
     return;
   },
 };
 
-function log(args) {
-  if (sails.config.environment === 'production') {
-    sails.log.info(...args);
-  } else {
-    console.log(...args);
-  }
-}
+// function log(args) {
+//   if (sails.config.environment === 'production') {
+//     sails.log.info(...args);
+//   } else {
+//     console.log(...args);
+//   }
+// }
