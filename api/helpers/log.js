09cd108b21af4f9d46f700e0b0ee5a06ec697549
Taylor Murray <taylor@goodparty.org>
2024-01-18 17:44:55 -0800

    fix: log function.

--- api/helpers/log.js
+++ api/helpers/log.js
@@ -22,11 +22,14 @@ module.exports = {
     try {
       const { arg1, arg2, arg3 } = inputs;
       if (arg1 && arg2 && arg3) {
-        console.log([`[${inputs.slug}]`, arg1, arg2, arg3]);
+        // log([`[${inputs.slug}]`, arg1, arg2, arg3]);
+        console.log(`[${inputs.slug}]`, arg1, arg2, arg3);
       } else if (arg1 && arg2) {
-        console.log([`[${inputs.slug}]`, arg1, arg2]);
+        // log([`[${inputs.slug}]`, arg1, arg2]);
+        console.log(`[${inputs.slug}]`, arg1, arg2);
       } else if (arg1) {
-        console.log([`[${inputs.slug}]`, arg1]);
+        //log([`[${inputs.slug}]`, arg1]);
+        console.log(`[${inputs.slug}]`, arg1);
       }
     } catch (e) {}
     return;
