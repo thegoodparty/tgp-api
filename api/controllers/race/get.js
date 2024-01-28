62095a25d40449fcbe291bd5d451e0502fe5a277
tomer-tgp <tomer@goodparty.org>
2024-01-27 20:20:22 -0800

    feat: office pages

--- api/controllers/race/get.js
+++ api/controllers/race/get.js
@@ -32,9 +32,85 @@ module.exports = {
       const race = await BallotRace.findOne({ hashId: id })
         .populate('municipality')
         .populate('county');
+      const { name, level } = await sails.helpers.ballotready.extractLocation(
+        race.data,
+      );
+
+      let otherRaces = [];
+      if (race.municipality) {
+        otherRaces = await BallotRace.find({
+          where: { municipality: race.municipality.id },
+          select: ['data', 'hashId'],
+        });
+      } else if (race.county) {
+        otherRaces = await BallotRace.find({
+          where: { county: race.county.id },
+          select: ['data', 'hashId'],
+        });
+      }
+      otherRaces = otherRaces.map((race) => {
+        return {
+          name: race.data.position_name,
+          hashId: race.hashId,
+        };
+      });
+
+      const {
+        election_name,
+        position_name,
+        election_day,
+        state,
+        partisan_type,
+        salary,
+        employment_type,
+        filing_date_start,
+        filing_date_end,
+        normalized_position_name,
+        position_description,
+        frequency,
+        filing_office_address,
+        filing_phone_number,
+        paperwork_instructions,
+        filing_requirements,
+        eligibility_requirements,
+        is_runoff,
+      } = race.data;
+
+      const filtered = {
+        id,
+        positionName: position_name,
+        locationName: name,
+        electionDate: election_day,
+        electionName: election_name,
+        state,
+        level,
+        partisanType: partisan_type,
+        salary,
+        employmentType: employment_type,
+        filingDateStart: filing_date_start,
+        filingDateEnd: filing_date_end,
+        normalizedPositionName: normalized_position_name,
+        positionDescription: position_description,
+        frequency,
+        subAreaName: race.subAreaName,
+        subAreaValue: race.subAreaValue,
+        filingOfficeAddress: filing_office_address,
+        filingPhoneNumber: filing_phone_number,
+        paperworkInstructions: paperwork_instructions,
+        filingRequirements: filing_requirements,
+        eligibilityRequirements: eligibility_requirements,
+        isRunoff: is_runoff,
+        municipality: race.municipality
+          ? { name: race.municipality.name, slug: race.municipality.slug }
+          : null,
+        county: race.county
+          ? { name: race.county.name, slug: race.county.slug }
+          : null,
+      };
 
       return exits.success({
-        race,
+        race: filtered,
+        otherRaces,
       });
     } catch (e) {
       console.log('error at races/by-city', e);
