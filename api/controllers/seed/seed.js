d5bce8de5d1444e5d77d955d3c08ac0e23e8f820
tomer-tgp <tomer@goodparty.org>
2024-02-06 23:14:22 -0800

    fix: seed

--- api/controllers/seed/seed.js
+++ api/controllers/seed/seed.js
@@ -4,9 +4,10 @@ module.exports = {
   exits: {},
 
   async fn(inputs, exits) {
+    let count = 0;
     try {
       // migrate pledged candidates with old onboarding to active candidate
-      const pledged = await Campaign.find({ isActive: false });
+      const pledged = await Campaign.find({ isActive: false }).populate('user');
       for (let i = 0; i < pledged.length; i++) {
         const campaign = pledged[i];
         const { data } = campaign;
@@ -20,10 +21,12 @@ module.exports = {
               currentStep: 'onboarding-complete',
             },
           });
+          await sendMail(campaign);
+          count++;
         }
       }
       return exits.success({
-        message: 'ok',
+        message: `updated ${count} campaigns`,
       });
     } catch (e) {
       console.log('Error in seed', e);
@@ -35,3 +38,21 @@ module.exports = {
     }
   },
 };
+
+async function sendMail(campaign) {
+  try {
+    const { user } = campaign;
+    const variables = JSON.stringify({
+      name: `${user.name}`,
+      link: `${appBase}/dashboard/plan`,
+    });
+    await sails.helpers.mailgun.mailgunTemplateSender(
+      user.email,
+      'Full Suite of AI Campaign Tools Now Available',
+      'campagin-launch',
+      variables,
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
