6a31a7680da98fb349ba39032e6c99f624941b37
tomer-tgp <tomer@goodparty.org>
2024-03-13 23:29:56 -0700

    chore: deploy

--- api/helpers/mailgun/mailgun-template-sender.js
+++ api/helpers/mailgun/mailgun-template-sender.js
@@ -57,7 +57,6 @@ module.exports = {
       }
 
       await mg.messages.create(domain, data);
-      console.log('sent');
       return exits.success({ message: 'email sent successfully' });
     } catch (e) {
       await sails.helpers.slack.errorLoggerHelper(
