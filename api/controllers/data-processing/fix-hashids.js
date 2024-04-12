25bac3469d72bf6b40f916a27df89d7e327446ea
Taylor Murray <taylor@goodparty.org>
2024-04-12 10:58:41 -0700

    chore: update fix-hashids

--- api/controllers/data-processing/fix-hashids.js
+++ api/controllers/data-processing/fix-hashids.js
@@ -14,18 +14,28 @@ module.exports = {
   fn: async function (inputs, exits) {
     try {
       // this script updates every single BallotRace with a hashid.
-      let races = await BallotRace.find({});
-      for (const race of races) {
-        const hashid = await sails.helpers.ballotready.encodeId(
-          race.ballotId,
-          'PositionElection',
-        );
-        await BallotRace.updateOne({
-          id: race.id,
-        }).set({
-          ballotHashId: hashid,
-        });
-        console.log(`updated race ${race.id} with hashid ${hashid}`);
+      let totalRaces = await BallotRace.count();
+      console.log('Total races: ', totalRaces);
+      let offset = 0;
+      let limit = 1000;
+      let updated = 0;
+      while (offset < totalRaces) {
+        let races = await BallotRace.find({}).limit(limit).skip(offset);
+        for (const race of races) {
+          const hashid = await sails.helpers.ballotready.encodeId(
+            race.ballotId,
+            'PositionElection',
+          );
+          await BallotRace.updateOne({
+            id: race.id,
+          }).set({
+            ballotHashId: hashid,
+          });
+          updated++;
+          console.log(
+            `updated race ${race.id} with hashid ${hashid}. updated: ${updated}`,
+          );
+        }
       }
 
       return exits.success({
