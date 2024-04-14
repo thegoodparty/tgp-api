a5ee2db65e3a6cacb1be808ffd0a620652a9e4e3
tomer-tgp <tomer@goodparty.org>
2024-04-14 13:12:58 -0700

    feat: WEB-1772

---
+++ api/controllers/campaign/doorKnocking/update.js
@@ -0,0 +1,58 @@
+module.exports = {
+  inputs: {
+    name: {
+      type: 'string',
+      required: true,
+    },
+    slug: {
+      type: 'string',
+      required: true,
+    },
+    endDate: {
+      type: 'string',
+      required: true,
+    },
+  },
+
+  exits: {
+    success: {
+      description: 'found',
+      responseType: 'ok',
+    },
+    badRequest: {
+      description: 'badRequest',
+      responseType: 'badRequest',
+    },
+  },
+  fn: async function (inputs, exits) {
+    try {
+      const { name, endDate, slug } = inputs;
+
+      const user = this.req.user;
+
+      const campaign = await sails.helpers.campaign.byUser(user);
+      if (!campaign) {
+        return exits.badRequest('No campaign');
+      }
+
+      const dkCampaign = await DoorKnockingCampaign.findOne({
+        slug,
+        campaign: campaign.id,
+      });
+      await DoorKnockingCampaign.updateOne({ id: dkCampaign.id }).set({
+        data: {
+          ...dkCampaign.data,
+          name,
+          endDate,
+        },
+      });
+
+      return exits.success({
+        slug,
+      });
+    } catch (e) {
+      console.log('Error at doorKnocking/create', e);
+      return exits.badRequest({ message: 'Error creating campaign.' });
+    }
+  },
+};
