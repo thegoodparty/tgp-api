b66ccd0f1d169f67f2d79f16d73c668bbb84d966
tomer-tgp <tomer@goodparty.org>
2024-05-06 19:23:31 -0400

    chore: delete existing voters first.

--- api/helpers/campaign/voter-data-helper.js
+++ api/helpers/campaign/voter-data-helper.js
@@ -148,6 +148,9 @@ module.exports = {
       //   .tolerate('E_UNIQUE');
 
       if (voterObjs.length > 0) {
+        // first remove all previous voters
+        await Campaign.replaceCollection(campaignId, 'voters').members([]);
+
         for (const voterObj of voterObjs) {
           try {
             const existing = await Voter.findOne({
