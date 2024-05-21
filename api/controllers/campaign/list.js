0522f8cc9271bdfccc20fd858679d151f591b627
Matthew Marcus <lurchbulldog@gmail.com>
2024-05-20 21:17:19 -0500

    [WEB-1981] - Admin campaign table filtered view (#191)
    * WEB-1981 - statistics
    
    * chore: adding join
    
    * [WEB-1981] - abstracts out where clause
    
    ---------
    
    Co-authored-by: tomer-tgp <tomer@goodparty.org>

--- api/controllers/campaign/list.js
+++ api/controllers/campaign/list.js
@@ -1,34 +1,106 @@
-/**
- * incumbents/find.js
- *
- * @description :: Find all Presidential Campaigns.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
+const buildQueryWhereClause = ({
+  state,
+  slug,
+  level,
+  primaryElectionDateStart,
+  primaryElectionDateEnd,
+  campaignStatus,
+  generalElectionDateStart,
+  generalElectionDateEnd,
+}) => `
+  ${slug ? ` AND campaign.slug = '${slug}'` : ''}
+  ${state ? ` AND campaign.details->>'state' = '${state}'` : ''}
+  ${level ? ` AND campaign.details->>'ballotLevel' = '${level.toUpperCase()}'` : ''}
+  ${campaignStatus ? ` AND campaign."isActive" = ${campaignStatus === 'active' ? 'true' : 'false'}` : ''}
+  ${primaryElectionDateStart ? ` AND campaign.details->>'primaryElectionDate' >= '${primaryElectionDateStart}'` : ''}
+  ${primaryElectionDateEnd ? ` AND campaign.details->>'primaryElectionDate' <= '${primaryElectionDateEnd}'` : ''}
+  ${generalElectionDateStart ? ` AND campaign.details->>'electionDate' >= '${generalElectionDateStart}'` : ''}
+  ${generalElectionDateEnd ? ` AND campaign.details->>'electionDate' <= '${generalElectionDateEnd}'` : ''}
+`;
 
 module.exports = {
   friendlyName: 'List of onboarding (Admin)',
 
-  inputs: {},
-
-  exits: {
-    success: {
-      description: 'Onboardings Found',
-      responseType: 'ok',
-    },
-    forbidden: {
-      description: 'Unauthorized',
-      responseType: 'forbidden',
+  inputs: {
+    state: {
+      type: 'string',
+    }, slug: {
+      type: 'string',
+    }, level: {
+      type: 'string',
+    }, primaryElectionDateStart: {
+      type: 'string',
+    }, primaryElectionDateEnd: {
+      type: 'string',
+    }, campaignStatus: {
+      type: 'string',
+    }, generalElectionDateStart: {
+      type: 'string',
+    }, generalElectionDateEnd: {
+      type: 'string',
     },
   },
 
-  fn: async function (inputs, exits) {
+  exits: {
+    success: {
+      description: 'Onboardings Found', responseType: 'ok',
+    }, forbidden: {
+      description: 'Unauthorized', responseType: 'forbidden',
+    },
+  },
+
+  fn: async function({
+    state,
+    slug,
+    level,
+    primaryElectionDateStart,
+    primaryElectionDateEnd,
+    campaignStatus,
+    generalElectionDateStart,
+    generalElectionDateEnd,
+  }, exits) {
     try {
-      const campaigns = await Campaign.find({
-        where: { user: { '!=': null } },
-      }).populate('user');
+      if (
+        !state &&
+        !slug &&
+        !level &&
+        !primaryElectionDateStart &&
+        !primaryElectionDateEnd &&
+        !campaignStatus &&
+        !generalElectionDateStart &&
+        !generalElectionDateEnd
+      ) {
+        const campaigns = await Campaign.find({
+          where: { user: { '!=': null } },
+        }).populate('user');
+        return exits.success({
+          campaigns,
+        });
+      }
+
+      const query = `
+        SELECT campaign.*, "user".*
+        FROM public.campaign
+        JOIN public."user" ON "user".id = campaign.user
+        WHERE campaign.user IS NOT NULL
+        ${
+          buildQueryWhereClause({
+            state,
+            slug,
+            level,
+            primaryElectionDateStart,
+            primaryElectionDateEnd,
+            campaignStatus,
+            generalElectionDateStart,
+            generalElectionDateEnd,
+          })
+        }
+        ORDER BY campaign.id DESC;`;
+
+      const campaigns = await sails.sendNativeQuery(query);
 
       return exits.success({
-        campaigns,
+        campaigns: campaigns?.rows || [],
       });
     } catch (e) {
       console.log('Error in onboarding list', e);
