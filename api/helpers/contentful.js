451f742dd8be1e21a9b67adccdfb14584165f4bb
tomer-tgp <tomer@goodparty.org>
2024-06-20 22:57:10 -0400

    feat: WEB-2551

--- api/helpers/contentful.js
+++ api/helpers/contentful.js
@@ -194,6 +194,16 @@ function mapResponse(items) {
           fields: item.fields,
           id: elementId,
         });
+      } else if (itemId === 'redirects') {
+        if (!mappedResponse.redirects) {
+          mappedResponse.redirects = [];
+        }
+        console.log('item', item);
+        mappedResponse.redirects.push({
+          id: elementId,
+          pathname: item.fields.pathname,
+          redirectUrl: item.fields.redirectUrl,
+        });
       } else if (itemId === 'blogSection') {
         if (!mappedResponse.blogSections) {
           mappedResponse.blogSections = [];
