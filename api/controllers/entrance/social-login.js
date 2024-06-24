0013b4cbb4e814ae76b4c74ba5c495d16016aa81
Taylor Murray <taylor@goodparty.org>
2024-06-24 12:32:23 -0700

    fix: add token cookie for social login.

--- api/controllers/entrance/social-login.js
+++ api/controllers/entrance/social-login.js
@@ -121,6 +121,13 @@ module.exports = {
         email: lowerCaseEmail,
       });
 
+      this.res.cookie('token', token, {
+        domain: '.goodparty.org', // Root domain
+        secure: process.env.NODE_ENV === 'production', // Ensures the cookie is only sent over HTTPS
+        httpOnly: true, // Ensures the cookie is only accessible via HTTP(S), not JavaScript
+        sameSite: 'None', // Allows the cookie to be sent with cross-site requests
+      });
+
       return exits.success({
         user,
         token,
