7e66dabf9cab9b3291b0fc25f0cf25e9dc8ce849
Matthew Marcus <lurchbulldog@gmail.com>
2024-06-27 15:42:40 -0500

    [WEB-2111] - Pro Subscription Confirmation Email

--- api/controllers/payments/checkout-session/update.js
+++ api/controllers/payments/checkout-session/update.js
@@ -1,8 +1,43 @@
+const {
+  getFormattedDateString,
+  formatUSDateString,
+  convertISO8601DateStringToUSDateString,
+} = require('../../../utils/dates');
 const stripe = require('stripe')(
   sails.config.custom.stripeSecretKey || sails.config.stripeSecretKey,
 );
 
-const appBase = sails.config.custom.appBase || sails.config.appBase;
+const sendProConfirmationEmail = async (user, campaign) => {
+  const { details: campaignDetails } = campaign;
+  const { electionDate: ISO8601DateString } = campaignDetails;
+
+  const formattedCurrentDate = getFormattedDateString(new Date());
+  const electionDate =
+    ISO8601DateString &&
+    formatUSDateString(
+      convertISO8601DateStringToUSDateString(ISO8601DateString),
+    );
+
+  const emailVars = {
+    userFullName: await sails.helpers.user.name(user),
+    startDate: formattedCurrentDate,
+    ...(electionDate ? { electionDate } : {}),
+  };
+
+  try {
+    await sails.helpers.mailgun.mailgunTemplateSender(
+      user.email,
+      `Welcome to Pro! Let's Empower Your Campaign Together`,
+      'pro-confirmation',
+      JSON.stringify(emailVars),
+    );
+  } catch (e) {
+    await sails.helpers.slack.errorLoggerHelper(
+      'Error sending pro confirmation email',
+      e,
+    );
+  }
+};
 
 module.exports = {
   inputs: {},
@@ -43,6 +78,12 @@ module.exports = {
     }).set({
       metaData: JSON.stringify(metaData),
     });
+
+    const campaign = await sails.helpers.campaign.byUser(user);
+
+    // TODO: This REALLY should be sent on receiving the relevant webhook event from the payment processor
+    //  Only placing here because we don't have anywhere better to put it right now
+    await sendProConfirmationEmail(user, campaign);
 
     return exits.success();
   },
