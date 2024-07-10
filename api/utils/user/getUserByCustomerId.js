0077abd2ae65f2714c1d645be1d2d686090255ac
Matthew Marcus <lurchbulldog@gmail.com>
2024-07-10 13:34:23 -0500

    [WEB-2266] - Fix user lookup by customerId query

--- api/utils/user/getUserByCustomerId.js
+++ api/utils/user/getUserByCustomerId.js
@@ -1,6 +1,9 @@
 const getUserByCustomerId = async (customerId) => {
+  // TODO: user.metaData field is currently just a text-type field. It should be a JSONb field
+  //  so that we can query it more efficiently and w/o haveing to use JSON.parse and JSON.stringify
+  //  all over the place.
   const rawQuery = `
-    SELECT * FROM "user" WHERE "metaData"->>'customerId' = '${customerId}';
+    SELECT * FROM "user" AS u WHERE u."metaData" LIKE '%${customerId}%';
   `;
   console.log(`rawQuery =>`, rawQuery);
   const { rows = [] } = await sails.getDatastore().sendNativeQuery(rawQuery);
