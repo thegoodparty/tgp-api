a4b21f6febb1846bfbebe16cbb7fd0d502b28dad
Matthew Marcus <lurchbulldog@gmail.com>
2024-07-10 22:42:07 -0500

    [WEB-2460] - Adds Slack messages to inform Politics team of Pro Plan sign-ups/cancellations/resumptions

--- api/utils/payments/webhookHandlers/checkoutSessionCompletedEventHandler.js
+++ api/utils/payments/webhookHandlers/checkoutSessionCompletedEventHandler.js
@@ -5,6 +5,7 @@ const {
 const { setUserCampaignIsPro } = require('../../campaign/setUserCampaignIsPro');
 const { sendProConfirmationEmail } = require('../sendProConfirmationEmail');
 const { doVoterDownloadCheck } = require('../../campaign/doVoterDownloadCheck');
+const { appEnvironment, PRODUCTION_ENV } = require('../../appEnvironment');
 const checkoutSessionCompletedEventHandler = async (event) => {
   const session = event.data.object;
   const { customer: customerId, subscription: subscriptionId } = session;
@@ -22,10 +23,18 @@ const checkoutSessionCompletedEventHandler = async (event) => {
     throw 'No user found with given checkout session userId';
   }
   const campaign = await sails.helpers.campaign.byUser(user);
+  const name = `${user.firstName}${user.firstName ? ` ${user.lastName}` : ''}`;
   await Promise.allSettled([
-    patchUserMetaData(user, { customerId, checkoutSession: null }),
+    patchUserMetaData(user, { customerId, checkoutSessionId: null }),
     setCampaignSubscriptionId(campaign, subscriptionId),
     setUserCampaignIsPro(campaign),
+    sails.helpers.slack.slackHelper(
+      {
+        title: 'New Pro User!',
+        body: `PRO PLAN SIGN UP:\`${name}\` w/ email ${user.email} and campaign slug \`${campaign.slug}\` has signed up for a pro subscription!`,
+      },
+      appEnvironment === PRODUCTION_ENV ? 'politics' : 'dev',
+    ),
     sendProConfirmationEmail(user, campaign),
     doVoterDownloadCheck(campaign),
   ]);
