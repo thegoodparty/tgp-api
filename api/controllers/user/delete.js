7001c8c2be1f9f8bc6d942abf3116916867d2621
Matthew Marcus <lurchbulldog@gmail.com>
2024-10-07 14:40:12 -0500

    [WEB-3080] - support for deleting users who are associated w/ campaigns

--- api/controllers/user/delete.js
+++ api/controllers/user/delete.js
@@ -2,6 +2,44 @@ const {
   handleCancelCampaign,
 } = require('../../utils/campaign/event-handlers/handleCancelCampaign');
 const { patchUserMetaData } = require('../../utils/user/patchUserMetaData');
+
+const handleDeleteCandidateAssociatedData = async (user, campaign) => {
+  const { details } = campaign || {};
+  const { subscriptionId } = details || {};
+  if (subscriptionId) {
+    try {
+      await handleCancelCampaign(campaign);
+    } catch (e) {
+      console.error(e);
+      await sails.helpers.slack.errorLoggerHelper('Error user/delete', e);
+      return e;
+    }
+  }
+  await CandidatePosition.destroy({
+    campaign: campaign?.id,
+  });
+
+  await PathToVictory.destroyOne({ id: campaign.pathToVictory?.id });
+  await CampaignVolunteer.destroy({
+    campaign: campaign?.id,
+  });
+  await CampaignRequest.destroy({
+    campaign: campaign?.id,
+  });
+  await Campaign.destroyOne({ id: campaign?.id });
+};
+
+const handleDeleteTeamMemberAssociatedData = async (user, campaign) => {
+  await CampaignVolunteer.destroy({
+    campaign: campaign.id,
+    user: user.id,
+  });
+  await CampaignRequest.destroy({
+    campaign: campaign.id,
+    user: user.id,
+  });
+};
+
 module.exports = {
   friendlyName: 'Delete User',
 
@@ -29,47 +67,24 @@ module.exports = {
       await ShareCandidate.destroy({ user: user.id });
       await Application.destroy({ user: user.id });
       const campaign = await sails.helpers.campaign.byUser(user.id);
-      campaign.user = await User.findOne({ id: user.id });
-      const { details } = campaign || {};
-      const { subscriptionId } = details || {};
-      await patchUserMetaData(user, { isDeleted: true });
-      if (subscriptionId) {
-        try {
-          await handleCancelCampaign(campaign);
-        } catch (e) {
-          console.error(e);
-          await sails.helpers.slack.errorLoggerHelper('Error user/delete', e);
-          return e;
-        }
+
+      if (!campaign) {
+        console.log('No campaign found for user. Deleting user.');
+        await User.destroyOne({ id: user.id });
+        return exits.success({
+          message: 'deleted successfully',
+        });
       }
-      // TODO: this needs to support a CM deleting their own account,
-      //  and should NOT delete things on the associated campaign.
-      await CandidatePosition.destroy({
-        campaign: campaign?.id,
-      });
-      // TODO: Same here.
-      await PathToVictory.destroyOne({ id: campaign.pathToVictory?.id });
-      await CampaignVolunteer.destroy({
-        or: [
-          {
-            user: user.id,
-          },
-          {
-            campaign: campaign?.id,
-          },
-        ],
-      });
-      await CampaignRequest.destroy({
-        or: [
-          {
-            user: user.id,
-          },
-          {
-            campaign: campaign?.id,
-          },
-        ],
-      });
-      await Campaign.destroyOne({ id: campaign?.id });
+
+      await patchUserMetaData(user, { isDeleted: true });
+
+      if (user.id === campaign.user) {
+        await handleDeleteCandidateAssociatedData(user, campaign);
+      } else {
+        await handleDeleteTeamMemberAssociatedData(user, campaign);
+      }
+
+      await Notification.destroy({ user: user.id });
       await User.destroyOne({ id: user.id });
       return exits.success({
         message: 'deleted successfully',
