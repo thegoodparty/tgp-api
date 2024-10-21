68ccb2b4ae31edf59a6616b288f27bad98544a44
Matthew Marcus <lurchbulldog@gmail.com>
2024-10-21 16:18:53 -0500

    [WEB-3193] - sets auto cancel to day of elections, removes logic for reminder email,  and keeps Slack message from posting (#289)

--- api/utils/payments/cancelCampaignProSubscription.js
+++ api/utils/payments/cancelCampaignProSubscription.js
@@ -4,23 +4,28 @@ const {
 const { setUserCampaignIsPro } = require('../campaign/setUserCampaignIsPro');
 const { appEnvironment, PRODUCTION_ENV } = require('../appEnvironment');
 
-const cancelCampaignProSubscription = async function (campaign, user) {
+const cancelCampaignProSubscription = async function (
+  campaign,
+  user,
+  autoCancel = false,
+) {
   const name = await sails.helpers.user.name(user);
   const { details } = campaign;
   const { office, otherOffice } = details || {};
   await setCampaignSubscriptionId(campaign, null);
   await setUserCampaignIsPro(campaign, false);
-  await sails.helpers.slack.slackHelper(
-    {
-      title: 'Pro Plan Cancellation',
-      body: `PRO PLAN CANCELLATION: \`${name}\` w/ email ${
-        user.email
-      }, running for '${otherOffice || office}' and campaign slug \`${
-        campaign.slug
-      }\` ended their pro subscription!`,
-    },
-    appEnvironment === PRODUCTION_ENV ? 'politics' : 'dev',
-  );
+  !autoCancel &&
+    (await sails.helpers.slack.slackHelper(
+      {
+        title: 'Pro Plan Cancellation',
+        body: `PRO PLAN CANCELLATION: \`${name}\` w/ email ${
+          user.email
+        }, running for '${otherOffice || office}' and campaign slug \`${
+          campaign.slug
+        }\` ended their pro subscription!`,
+      },
+      appEnvironment === PRODUCTION_ENV ? 'politics' : 'dev',
+    ));
 };
 
 module.exports = {
