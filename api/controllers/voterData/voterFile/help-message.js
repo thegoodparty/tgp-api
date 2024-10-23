97f12b0ed904b03d0bd2e5e52747078971a258c3
Sanjeev Nayak <oldfashionedrobot@users.noreply.github.com>
2024-10-22 22:40:21 -0400

    WEB-3027: Enhancement: Ability to tag Users in Slack with automated messages: Globally (#290)
    * WEB-3027: updated slack messaging to include PA user mentions, also updated formatting of slack JSON for size limits
    
    * update comment
    
    * WEB-3209: Custom Voter File not Saving correctly (#291)
    
    * WEB-3209: make campaign schedule voter file link include campaign slug; allow admin to get voter files by campaign slug
    
    * WEB-3209: dont throw error, fallback to user campaign instead
    
    ---------
    
    Co-authored-by: Sanjeev Nayak <snayak00@gmail.com>
    
    ---------
    
    Co-authored-by: Sanjeev Nayak <snayak00@gmail.com>

--- api/controllers/voterData/voterFile/help-message.js
+++ api/controllers/voterData/voterFile/help-message.js
@@ -3,7 +3,9 @@ const {
   PRODUCTION_ENV,
 } = require('../../../utils/appEnvironment');
 
-const { getCrmCompanyOwnerName } = require('../../../utils/crm/getCrmCompanyOwnerName.js');
+const {
+  getCrmCompanyOwnerName,
+} = require('../../../utils/crm/getCrmCompanyOwnerName.js');
 
 module.exports = {
   inputs: {
@@ -35,37 +37,29 @@ module.exports = {
       const campaign = await sails.helpers.campaign.byUser(user.id);
       const { details, tier } = campaign;
       const crmCompany = await sails.helpers.crm.getCompany(campaign);
-      const assignedPa = await getCrmCompanyOwnerName(crmCompany);
-      const candidateOffice = details.office?.toLowerCase().trim() === 'other' ? details.otherOffice : details.office;
+      const assignedPa = await getCrmCompanyOwnerName(crmCompany, true);
+      const candidateOffice =
+        details.office?.toLowerCase().trim() === 'other'
+          ? details.otherOffice
+          : details.office;
+
+      const slackBlocks = buildSlackBlocks({
+        name: `${firstName} ${lastName}`,
+        email,
+        phone,
+        office: candidateOffice,
+        state: details.state,
+        tier,
+        type,
+        message,
+        assignedPa,
+        crmCompanyId: crmCompany?.id,
+      });
 
       await sails.helpers.slack.slackHelper(
-        {
-          title: '🚨*Voter File Assistance Request*🚨',
-          body: `🚨*Voter File Assistance Request*🚨
-
-*Candidate/User:*
-￮ Name: ${firstName} ${lastName} 
-￮ Email: ${email}
-￮ Phone: ${phone}
-
-Office: ${candidateOffice}
-State: ${details.state}
-Viability Tier: ${tier}
-
-Type: ${type}
-
-*Message from Candidate*: ${message}
-
-*Assigned PA:*
-  ${assignedPa || 'None Assigned'}
-      ${
-        crmCompany?.id
-          ? `https://app.hubspot.com/contacts/21589597/record/0-2/${crmCompany.id}`
-          : 'No CRM company found'
-      }
-`,
-        },
+        slackBlocks,
         appEnvironment === PRODUCTION_ENV ? 'politics' : 'dev',
+        false,
       );
 
       return exits.success({ message: 'ok' });
@@ -75,3 +69,248 @@ Type: ${type}
     }
   },
 };
+
+function buildSlackBlocks({
+  name,
+  email,
+  phone,
+  office,
+  state,
+  tier,
+  type,
+  message,
+  assignedPa,
+  crmCompanyId,
+}) {
+  return {
+    blocks: [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: '🚨 Voter File Assistance Request 🚨',
+          emoji: true,
+        },
+      },
+      {
+        type: 'rich_text',
+        elements: [
+          {
+            type: 'rich_text_section',
+            elements: [
+              {
+                type: 'emoji',
+                name: 'gp',
+              },
+              {
+                type: 'text',
+                text: ' Candidate/User:',
+                style: {
+                  bold: true,
+                },
+              },
+            ],
+          },
+          {
+            type: 'rich_text_list',
+            style: 'bullet',
+            elements: [
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'Name: ',
+                    style: {
+                      bold: true,
+                    },
+                  },
+                  {
+                    type: 'text',
+                    text: String(name),
+                  },
+                ],
+              },
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'Email: ',
+                    style: {
+                      bold: true,
+                    },
+                  },
+                  {
+                    type: 'text',
+                    text: String(email),
+                  },
+                ],
+              },
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'Phone: ',
+                    style: {
+                      bold: true,
+                    },
+                  },
+                  {
+                    type: 'text',
+                    text: String(phone),
+                  },
+                ],
+              },
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'Office: ',
+                    style: {
+                      bold: true,
+                    },
+                  },
+                  {
+                    type: 'text',
+                    text: String(office),
+                  },
+                ],
+              },
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'State: ',
+                    style: {
+                      bold: true,
+                    },
+                  },
+                  {
+                    type: 'text',
+                    text: String(state),
+                  },
+                ],
+              },
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'Viability Tier: ',
+                    style: {
+                      bold: true,
+                    },
+                  },
+                  {
+                    type: 'text',
+                    text: String(tier),
+                  },
+                ],
+              },
+              {
+                type: 'rich_text_section',
+                elements: [
+                  {
+                    type: 'text',
+                    text: 'Type: ',
+                    style: {
+                      bold: true,
+                    },
+                  },
+                  {
+                    type: 'text',
+                    text: String(type),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: 'rich_text_section',
+            elements: [
+              {
+                type: 'text',
+                text: '\n\n',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: 'rich_text',
+        elements: [
+          {
+            type: 'rich_text_section',
+            elements: [
+              {
+                type: 'emoji',
+                name: 'speech_balloon',
+              },
+              {
+                type: 'text',
+                text: ' Message from Candidate:',
+                style: {
+                  bold: true,
+                },
+              },
+            ],
+          },
+          {
+            type: 'rich_text_quote',
+            elements: [
+              {
+                type: 'text',
+                text: String(message),
+              },
+            ],
+          },
+          {
+            type: 'rich_text_section',
+            elements: [
+              {
+                type: 'text',
+                text: '\n\n',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: 'rich_text',
+        elements: [
+          {
+            type: 'rich_text_section',
+            elements: [
+              {
+                type: 'emoji',
+                name: 'eyes',
+              },
+              {
+                type: 'text',
+                text: ' Assigned PA:',
+                style: {
+                  bold: true,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `${assignedPa || 'None Assigned'}\n${
+            crmCompanyId
+              ? `https://app.hubspot.com/contacts/21589597/record/0-2/${crmCompanyId}`
+              : 'No CRM company found'
+          }`,
+        },
+      },
+    ],
+  };
+}
--- api/controllers/voterData/voterFile/schedule.js
+++ api/controllers/voterData/voterFile/schedule.js
@@ -8,9 +8,8 @@ const {
   getCrmCompanyOwnerName,
 } = require('../../../utils/crm/getCrmCompanyOwnerName.js');
 
-const isUrl = require('validator/lib/isURL');
-
 const assetsBase = sails.config.custom.assetsBase || sails.config.assetsBase;
+const appBase = sails.config.custom.appBase || sails.config.appBase;
 
 module.exports = {
   inputs: {
@@ -36,9 +35,6 @@ module.exports = {
     },
     voicemail: {
       type: 'boolean',
-    },
-    voterFileUrl: {
-      type: 'string',
     },
     type: {
       type: 'string',
@@ -70,7 +66,7 @@ module.exports = {
         image,
         type,
       } = inputs;
-      let voterFileUrl = inputs.voterFileUrl;
+
       const { user } = this.req;
       const { firstName, lastName, email, phone } = user;
       const audience =
@@ -88,90 +84,90 @@ module.exports = {
         throw new Error(`crmCompany not found for ${campaign}`);
       }
 
-      const assignedPa = await getCrmCompanyOwnerName(crmCompany);
-      const messagingScript = campaign.aiContent[script]?.content
-        ? sanitizeHtml(campaign.aiContent[script]?.content, {
+      const assignedPa = await getCrmCompanyOwnerName(crmCompany, true);
+      const messagingScript = campaign.aiContent?.[script]?.content
+        ? sanitizeHtml(campaign.aiContent?.[script]?.content, {
             allowedTags: [],
             allowedAttributes: {},
           })
         : script;
 
-      if (
-        voterFileUrl &&
-        !isUrl(voterFileUrl) &&
-        !voterFileUrl.startsWith('http://localhost')
-      ) {
-        console.error('voterFileUrl is invalid');
+      // build Voter File URL
+      let voterFileUrl;
+
+      try {
+        const filters = [];
+        for (const key in audience) {
+          if (audience[key] === true) {
+            filters.push(key);
+          }
+        }
+        const voterFileRoute = sails.getUrlFor('voterData/voterFile/get');
+        const encodedFilters = encodeURIComponent(JSON.stringify({ filters }));
+        voterFileUrl = `${appBase}${voterFileRoute}?type=${type}&slug=${campaign.slug}&customFilters=${encodedFilters}`;
+      } catch (e) {
+        console.error('Error building voterFileUrl: ', e);
         voterFileUrl = null;
       }
 
+      // format audience filters for slack message
       const formattedAudience = Object.entries(audience)
         .map(([key, value]) => {
           if (key === 'audience_request') {
             return;
           }
-          return `￮ ${key}: ${value ? '✅ Yes' : '❌ No'}`;
+
+          return {
+            type: 'rich_text_section',
+            elements: [
+              {
+                type: 'text',
+                text: `${key}: `,
+                style: {
+                  bold: true,
+                },
+              },
+              {
+                type: 'text',
+                text: value ? '✅ Yes' : '❌ No',
+              },
+            ],
+          };
         })
         // eslint-disable-next-line eqeqeq
-        .filter((val) => val != undefined)
-        .join('\n');
+        .filter((val) => val != undefined);
 
       // Upload image
       const bucket = `${assetsBase}/scheduled-campaign/${campaign.slug}/${type}/${date}`;
       const response = await sails.helpers.images.uploadImage(image, bucket);
       const uploadedImage = response.data.files[0];
-      const imageUrl = `https://${bucket}/${uploadedImage}`;
+      const imageUrl = uploadedImage
+        ? `https://${bucket}/${uploadedImage}`
+        : null;
+
+      const slackBlocks = buildSlackBlocks({
+        name: `${firstName} ${lastName}`,
+        email,
+        phone,
+        assignedPa,
+        crmCompanyId: crmCompany?.id,
+        voterFileUrl,
+        type,
+        budget,
+        voicemail,
+        date,
+        script,
+        messagingScript,
+        imageUrl,
+        message,
+        formattedAudience,
+        audienceRequest: audience['audience_request'],
+      });
 
       await sails.helpers.slack.slackHelper(
-        {
-          title: '🚨Campaign Schedule Request🚨',
-          body: `🚨*Campaign Schedule Request*🚨
-
-*Candidate/User:*
-￮ Name: ${firstName} ${lastName}
-￮ Email: ${email}
-￮ Phone: ${phone}
-
-*Assigned Political Advisor (PA):*
-￮ Assigned PA:
-  ${assignedPa || 'None Assigned'}
-
-  ${
-    crmCompany?.id
-      ? `https://app.hubspot.com/contacts/21589597/record/0-2/${crmCompany.id}`
-      : 'No CRM company found'
-  }
-
-*Voter File Download Link:*
-${
-  voterFileUrl
-    ? `🔒 <${voterFileUrl}|Voter File Download>`
-    : 'Error: Not provided or invalid'
-}
-
-*Campaign Details:*
-￮ Campaign Type: ${type}
-￮ Budget: $${budget}
-￮ Scheduled Date: ${date}
-￮ Script Key: ${script}
-
-*Messaging Script:*
-\`\`\`
-${messagingScript}
-\`\`\`
-
-${uploadedImage ? `*Image File:*\n${imageUrl}\n` : ''}
-*Message From User:*
-￮ Message: ${message}
-
-*Audience Selection:*
-${formattedAudience}
-￮ Audience Request: ${audience['audience_request'] || 'N/A'}
-
-${voicemail !== undefined ? `￮ Voicemail: ${voicemail ? 'Yes' : 'No'}` : ''}
-`,
-        },
+        slackBlocks,
         appEnvironment === PRODUCTION_ENV ? 'politics' : 'dev',
+        false,
       );
 
       return exits.success({ message: 'ok' });
@@ -181,3 +177,446 @@ ${voicemail !== undefined ? `￮ Voicemail: ${voicemail ? 'Yes' : 'No'}` : ''}
     }
   },
 };
+
+function buildSlackBlocks({
+  name,
+  email,
+  phone,
+  assignedPa,
+  crmCompanyId,
+  voterFileUrl,
+  type,
+  budget,
+  voicemail,
+  date,
+  script,
+  messagingScript,
+  imageUrl,
+  message,
+  formattedAudience,
+  audienceRequest,
+}) {
+  const blocks = [
+    {
+      type: 'header',
+      text: {
+        type: 'plain_text',
+        text: '🚨 Campaign Schedule Request 🚨',
+        emoji: true,
+      },
+    },
+    {
+      type: 'rich_text',
+      elements: [
+        {
+          type: 'rich_text_section',
+          elements: [
+            {
+              type: 'emoji',
+              name: 'gp',
+            },
+            {
+              type: 'text',
+              text: ' Candidate/User:',
+              style: {
+                bold: true,
+              },
+            },
+          ],
+        },
+        {
+          type: 'rich_text_list',
+          style: 'bullet',
+          elements: [
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Name: ',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: String(name),
+                },
+              ],
+            },
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Email: ',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: String(email),
+                },
+              ],
+            },
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Phone: ',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: String(phone),
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'rich_text',
+      elements: [
+        {
+          type: 'rich_text_section',
+          elements: [
+            {
+              type: 'emoji',
+              name: 'zap',
+            },
+            {
+              type: 'text',
+              text: ' Campaign Details:',
+              style: {
+                bold: true,
+              },
+            },
+          ],
+        },
+        {
+          type: 'rich_text_list',
+          style: 'bullet',
+          elements: [
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Campaign Type: ',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: String(type),
+                },
+              ],
+            },
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Budget: ',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: '$' + Number(budget).toLocaleString(),
+                },
+              ],
+            },
+            // eslint-disable-next-line eqeqeq
+            voicemail != undefined
+              ? {
+                  type: 'rich_text_section',
+                  elements: [
+                    {
+                      type: 'text',
+                      text: 'Voicemail: ',
+                      style: {
+                        bold: true,
+                      },
+                    },
+                    {
+                      type: 'text',
+                      text: voicemail ? 'Yes' : 'No',
+                    },
+                  ],
+                }
+              : undefined,
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Scheduled Date: ',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: String(date),
+                },
+              ],
+            },
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Script Key: ',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: String(script),
+                },
+              ],
+            },
+            // eslint-disable-next-line eqeqeq
+          ].filter((elem) => elem != undefined),
+        },
+      ],
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'rich_text',
+      elements: [
+        {
+          type: 'rich_text_section',
+          elements: [
+            {
+              type: 'emoji',
+              name: 'eyes',
+            },
+            {
+              type: 'text',
+              text: ' Assigned Political Advisor (PA):',
+              style: {
+                bold: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `${assignedPa || 'None Assigned'}\n${
+          crmCompanyId
+            ? `https://app.hubspot.com/contacts/21589597/record/0-2/${crmCompanyId}`
+            : 'No CRM company found'
+        }`,
+      },
+    },
+    {
+      type: 'rich_text',
+      elements: [
+        {
+          type: 'rich_text_section',
+          elements: [
+            {
+              type: 'emoji',
+              name: 'lock',
+            },
+            {
+              type: 'text',
+              text: ' Voter File Download Link\n',
+              style: {
+                bold: true,
+              },
+            },
+            voterFileUrl
+              ? {
+                  type: 'link',
+                  text: 'Voter File Download',
+                  url: String(voterFileUrl),
+                }
+              : {
+                  type: 'text',
+                  text: 'Error: Not provided or invalid',
+                  style: {
+                    bold: true,
+                  },
+                },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'rich_text',
+      elements: [
+        {
+          type: 'rich_text_section',
+          elements: [
+            {
+              type: 'emoji',
+              name: 'scroll',
+            },
+            {
+              type: 'text',
+              text: ' AI-Generated Script:',
+              style: {
+                bold: true,
+              },
+            },
+          ],
+        },
+        {
+          type: 'rich_text_preformatted',
+          elements: [
+            {
+              type: 'text',
+              text: String(messagingScript),
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'rich_text',
+      elements: [
+        {
+          type: 'rich_text_section',
+          elements: [
+            {
+              type: 'emoji',
+              name: 'speech_balloon',
+            },
+            {
+              type: 'text',
+              text: ' Message From User:\n',
+              style: {
+                bold: true,
+              },
+            },
+          ],
+        },
+        {
+          type: 'rich_text_quote',
+          elements: [
+            {
+              type: 'text',
+              text: String(message),
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'rich_text',
+      elements: [
+        {
+          type: 'rich_text_section',
+          elements: [
+            {
+              type: 'emoji',
+              name: 'busts_in_silhouette',
+            },
+            {
+              type: 'text',
+              text: ' Audience Selection:',
+              style: {
+                bold: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'rich_text',
+      elements: [
+        {
+          type: 'rich_text_list',
+          style: 'bullet',
+          elements: [
+            ...formattedAudience,
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Audience Request: ',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: audienceRequest ? String(audienceRequest) : 'N/A',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'divider',
+    },
+    imageUrl
+      ? {
+          type: 'rich_text',
+          elements: [
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'emoji',
+                  name: 'floppy_disk',
+                },
+                {
+                  type: 'text',
+                  text: ' Image File:',
+                  style: {
+                    bold: true,
+                  },
+                },
+              ],
+            },
+          ],
+        }
+      : undefined,
+    imageUrl
+      ? {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: String(imageUrl),
+          },
+        }
+      : undefined,
+  ];
+
+  return {
+    // eslint-disable-next-line eqeqeq
+    blocks: blocks.filter((block) => block != undefined),
+  };
+}
