3bdcaa63e7c3c58c35c7258e40a4874262067f0e
Sanjeev Nayak <oldfashionedrobot@users.noreply.github.com>
2024-11-05 13:29:47 -0500

    WEB-3306: add ability to filter by win result to list-map-count (#302)
    Co-authored-by: Sanjeev Nayak <snayak00@gmail.com>

--- api/controllers/campaign/list-map-count.js
+++ api/controllers/campaign/list-map-count.js
@@ -7,6 +7,9 @@ module.exports = {
     state: {
       type: 'string',
     },
+    results: {
+      type: 'boolean',
+    },
   },
 
   exits: {
@@ -24,12 +27,36 @@ module.exports = {
     try {
       const isProd = appBase === 'https://goodparty.org';
 
-      const { state } = inputs;
+      const { state, results } = inputs;
 
-      const campaigns = await Campaign.find({
-        select: ['details', 'didWin', 'data', 'user'],
-        where: { user: { '!=': null }, isDemo: false, isActive: true },
-      }).populate('user');
+      let whereClauses = `WHERE c."user" IS NOT NULL AND c."isDemo" = false AND c."isActive" = true`;
+
+      if (state) {
+        whereClauses += ` AND c.details->>'state' = '${state}'`;
+      }
+
+      if (results) {
+        whereClauses += ` AND (c."didWin" = true OR c.data->'hubSpotUpdates'->>'election_results' = 'Won General')`; // "didWin" is properly quoted
+      }
+
+      // Native SQL query with proper column quoting and JOIN
+      const rawQuery = `
+       SELECT 
+         c."slug", 
+         c."details", 
+         c."didWin", 
+         c."data", 
+         u."firstName", 
+         u."lastName", 
+         u."avatar"
+       FROM "campaign" c
+       JOIN "user" u ON c."user" = u.id
+       ${whereClauses};
+     `;
+
+      const result = await sails.sendNativeQuery(rawQuery);
+
+      const campaigns = result.rows;
 
       const lastWeek = moment().subtract(7, 'days');
 
@@ -37,10 +64,9 @@ module.exports = {
       for (let i = 0; i < campaigns.length; i++) {
         const campaign = campaigns[i];
 
-        const { details, didWin, data, user } = campaign;
+        const { details, didWin, data } = campaign;
 
         if (
-          !user ||
           !details?.zip ||
           didWin === false ||
           !details?.geoLocation?.lng ||
@@ -48,9 +74,7 @@ module.exports = {
         ) {
           continue;
         }
-        if (state && details?.state !== state) {
-          continue;
-        }
+
         if (isProd) {
           if (data?.hubSpotUpdates?.verified_candidates !== 'Yes') {
             continue;
