f10dfa65c48a526be6f78620d367dabc44d784bf
tomer-tgp <tomer@goodparty.org>
2025-02-13 16:48:13 -0500

    fix: local

--- api/controllers/campaign/ballotData/races.js
+++ api/controllers/campaign/ballotData/races.js
@@ -160,7 +160,7 @@ function getRaceQuery(zip, level, electionDate, startCursor) {
   const gt = moment(electionDate).startOf('month').format('YYYY-MM-DD');
   const lt = moment(electionDate).endOf('month').format('YYYY-MM-DD');
   let levelWithTownship = level?.toUpperCase();
-  if (level === 'LOCAL OR TOWNSHIP' || level?.startsWith('LOCAL')) {
+  if (level === 'LOCAL') {
     levelWithTownship = 'LOCAL,TOWNSHIP';
   }
 
